b4d387b41d1aec5db215862835f23fea38583f17
Test Man <ttestman4@gmail.com>
2018-03-28 08:58:41 +0530

    Sample 2 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { Lib2Module } from './lib2/lib2.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Lib2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
